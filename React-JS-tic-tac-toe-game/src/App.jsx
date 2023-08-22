2939ccab5a2730a8f574218c77fb374aec904d7b
Yiim <ilyimyan@gmail.com>
2023-08-22 21:54:28 +0800

    initial commit

--- React-JS-tic-tac-toe-game/src/App.jsx
+++ React-JS-tic-tac-toe-game/src/App.jsx
@@ -1,11 +1,6 @@
 import { useState } from 'react'
 
-function Square() {
-  const [value, setValue] = useState('');
-
-  function handleClick() {
-    setValue('X');
-  }
+function Square({value}) {
 return  <button className='square' onClick={handleClick}>{value}</button>
 }
 
