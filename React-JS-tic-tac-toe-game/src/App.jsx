4c7e9c8edc430b0b6173ae0d926df5d154e8e8f4
Yiim <ilyimyan@gmail.com>
2023-08-22 22:34:08 +0800

    initial commit

--- React-JS-tic-tac-toe-game/src/App.jsx
+++ React-JS-tic-tac-toe-game/src/App.jsx
@@ -17,14 +17,14 @@ export default function Board() {
   return (
     <div className='board'>
     <Square value={squares[0]} onSquareClick={() => hanndleClick(0)} />
-    <Square value={squares[1]} />
-    <Square value={squares[2]} />
-    <Square value={squares[3]} />
-    <Square value={squares[4]} />
-    <Square value={squares[5]} />
-    <Square value={squares[6]} />
-    <Square value={squares[7]} />
-    <Square value={squares[8]} />
+    <Square value={squares[1]} onSquareClick={() => hanndleClick(1)} />
+    <Square value={squares[2]} onSquareClick={() => hanndleClick(2)} />
+    <Square value={squares[3]} onSquareClick={() => hanndleClick(3)} />
+    <Square value={squares[4]} onSquareClick={() => hanndleClick(4)} />
+    <Square value={squares[5]} onSquareClick={() => hanndleClick(5)} />
+    <Square value={squares[6]} onSquareClick={() => hanndleClick(6)} />
+    <Square value={squares[7]} onSquareClick={() => hanndleClick(7)} />
+    <Square value={squares[8]} onSquareClick={() => hanndleClick(8)} />
     </div>
   )
 }
